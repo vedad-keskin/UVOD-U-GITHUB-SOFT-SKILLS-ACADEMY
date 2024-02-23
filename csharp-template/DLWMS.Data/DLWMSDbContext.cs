fc0c11ccfe74a1f8646a30bda4b858835e4d1d35
Vedad <vedad.ribery@gmail.com>
2024-02-23 21:00:50 +0100

    c# template

---
+++ csharp-template/DLWMS.Data/DLWMSDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLWMS.Data
+{
+    public class DLWMSDbContext : DbContext
+    {
+        private string dbPutanja;
+
+	   public DLWMSDbContext()
+	   {
+            dbPutanja = ConfigurationManager.
+                ConnectionStrings["DLWMSPutanja"].ConnectionString;
+	   }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(dbPutanja);
+        }
+       
+        public DbSet<Student> Studenti { get; set; }
+        public DbSet<Spol> Spolovi { get; set; }
+    }
+}
---
+++ csharp-template/DLWMS.Data/Spol.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DLWMS.Data
+{
+
+    [Table("Spolovi")]
+    public class Spol
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+    }
+
+}
